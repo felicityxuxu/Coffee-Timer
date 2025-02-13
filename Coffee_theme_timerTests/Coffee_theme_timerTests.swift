661a3cb2daa7caa9791949b4dc9650f85b3bf2eb
felicityxuxu <felicityzyx@gmail.com>
2025-02-13 14:03:07 -0800

    Initial Commit

---
+++ Coffee_theme_timerTests/Coffee_theme_timerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_theme_timerTests.swift
+//  Coffee_theme_timerTests
+//
+//  Created by Yongxu Zhu on 2/13/25.
+//
+
+import Testing
+@testable import Coffee_theme_timer
+
+struct Coffee_theme_timerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
